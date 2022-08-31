0c37443de851e4f3150eb93bfcb54f7c3778b4d1
Brandon Cieplinski <bcieplinski0@gmail.com>
2022-08-31 15:08:14 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,9 +23,9 @@ export default function Home ({posts}: Props) {
     <Header/>
     <div className="flex justify-between items-center bg-yellow-400 border-black border-y py-10 lg:py-0">
 <div className="px-10 space-y-5">
-<h1 className="text-6xl m-w-xl font-serif"><span className="underline decoration-black decoration-4">Medium</span> is the place to write, read and connect</h1>
+<h1 className="text-6xl m-w-xl font-serif"><span className="underline decoration-black decoration-4">Medium</span> is the place to write read and connect</h1>
 <h2>
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book.
 </h2>
 </div>
 
