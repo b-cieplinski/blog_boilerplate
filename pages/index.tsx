ca89d133904ae76a85e2307efc26c4a7ed461038
Brandon Cieplinski <bcieplinski0@gmail.com>
2022-08-31 15:02:56 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home ({posts}: Props) {
 <div className="px-10 space-y-5">
 <h1 className="text-6xl m-w-xl font-serif"><span className="underline decoration-black decoration-4">Medium</span> is the place to write, read and connect</h1>
 <h2>
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
 </h2>
 </div>
 
